ccd8542cce711b0b357642a734d70c644441e5d2
Sakhila2001 <154709250+Sakhila2001@users.noreply.github.com>
2025-09-14 21:28:26 +0545

    swiper and autoplay in Home slider

--- src/components/HomeCatSlider/index.jsx
+++ src/components/HomeCatSlider/index.jsx
@@ -1,14 +1,169 @@
-import React from 'react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "../../App.css";
 
+import { Navigation } from "swiper/modules";
+import { Link } from "react-router-dom";
 const HomeCatSlider = () => {
   return (
-    <div className='homeCatSlider'>
-    <div className='container'>
-      
-    </div>
-      
+    <div className="homeCatSlider  pt-0 lg:pt-4 py-4 lg:py-8">
+      <div className="container">
+        <Swiper
+          slidesPerView={7}
+          spaceBetween={10}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-4 lg:py-7 px-3 bg-white rounbded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://serviceapi.spicezgold.com/download/1755610847575_file_1734525204708_fash.png"
+                  className="w-[40px] lg:w-[60px] transition-all"
+                />
+                <h3 className="text-[12px] lg:text-[15px] font-[500] mt-3">
+                  Fashion
+                </h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
-}
+};
 
 export default HomeCatSlider;
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -6,7 +6,7 @@ import { IoMdSearch } from "react-icons/io";
 const Search = () => {
   return (
     <div className='searchBox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[5px] relative p-2'>
-        <input type='text' placeholder='Search for products ....' className='w-full h-[35px] outline-none bg-inherit text-[15px] pt p-l' />
+        <input type='text' placeholder='Search for products ....' className='w-full h-[35px] focus:outline-none bg-inherit p-2 text-[15px]' />
         <Button className='!absolute !top-[8px] !right-[5px] !z-50 !w-[40px] !min-w-[40px] !h-[40px] !rounded-full !text-black'><IoMdSearch className='text-[#454545] text-[22px]' /> </Button>
         </div>
   )
